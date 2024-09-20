0e385d4301ac1ce97991a900e8890dc4bd66c03d
RoniPG <ronigmn@gmail.com>
2024-09-20 15:11:37 +0200

    Iteration 2 | Find the Longest Word

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -3,13 +3,28 @@ function maxOfTwoNumbers(number1, number2) {
     return Math.max(number1, number2)
 }
 
-
-
-
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+        return null
+    } else {
+        let longestWord
+        let max = 0
+        words.forEach(word => {
+            // console.log("word: ", word);
+            // console.log("word.length: ", word.length);
+            // console.log("word.length: ", word.length, " > max: ", max);
+            if (word.length > max) {
+                max = word.length
+                longestWord = word
+                // console.log("longestWord: ", longestWord);
+            }
+        });
+        return longestWord
+    }
+}
 
 
 
@@ -17,7 +32,7 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers() { }
 
 
 
@@ -25,7 +40,7 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers() { }
 
 
 
@@ -33,4 +48,4 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist() { }
